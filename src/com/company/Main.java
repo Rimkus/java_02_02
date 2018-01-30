6e6e770f08c3e32249f74d58b5a9a8fbfe2918fc
410 <karolispar@gmail.com>
2018-01-30 15:52:11 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,17 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("iveskite du skaicius");
+        System.out.println("iveskite savo svori ir ugi");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println("suma:" +suma(a,b));
-        System.out.println("skirtumas:" +skirtumas(a,b));
-        System.out.println("suma:" + suma((float) a, (float) b));
-        System.out.println("skirtumas:" + skirtumas((float) a, (float) b));
+        float svoris = scanner.nextFloat();
+        float ugis = scanner.nextFloat();
+        float KMY = svoris / (ugis * ugis);
+        System.out.println(KMY);
 
 
     }
-    public static int suma(int a, int b){
-        return a+b;
-
-    }
-    public static int skirtumas(int a, int b){
-        return a-b;
-
-    }
-    public static float suma(float a, float b){
-        return a+b;
-    }
-    public static float skirtumas(float a, float b){
-        return a-b;
-    }
-
-
-
 }
