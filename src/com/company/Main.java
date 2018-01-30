f9f654d8d0e8838803ba391f676359bde2e041fc
410 <karolispar@gmail.com>
2018-01-30 15:18:37 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("iveskite savo varda");
+        System.out.println("iveskite du skaicius");
         Scanner scanner = new Scanner(System.in);
-        String vardas = scanner.nextLine();
-        System.out.println("jusu vardas turi simboliu:" + vardas.length());
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("suma:" + suma(a,b));
+        System.out.println("skirtumas:" + skirtumas(a,b));
 	// wsrite your code here
+    }
+    public static int suma(int a, int b){
+        return a+b;
+    }
+    public static int skirtumas(int a, int b){
+        return a-b;
     }
 }
