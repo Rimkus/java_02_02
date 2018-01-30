1c59480f828b31fe4b203d482d1502cd93d679d8
410 <karolispar@gmail.com>
2018-01-30 15:44:19 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int suma = a+b;
-        int skirtumas = a-b;
-        System.out.println("suma:" +suma);
-        System.out.println("skirtumas:" + skirtumas);
+        System.out.println("suma:" +suma(a,b));
+        System.out.println("skirtumas:" +skirtumas(a,b));
+        System.out.println("suma:" + suma((float) a, (float) b));
+        System.out.println("skirtumas:" + skirtumas((float) a, (float) b));
 
 
     }
+    public static int suma(int a, int b){
+        return a+b;
+
+    }
+    public static int skirtumas(int a, int b){
+        return a-b;
+
+    }
+    public static float suma(float a, float b){
+        return a+b;
+    }
+    public static float skirtumas(float a, float b){
+        return a-b;
+    }
+
+
+
 }
